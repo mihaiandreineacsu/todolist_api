dc5791a911bbeef7a060194e8cf401fe1e3b3389
Mihai-Andrei Neacsu <crunck78@googlemail.com>
2022-08-19 16:29:07 +0200

    10 - Serializer - User Feld hinzufügen

--- todo/serializers.py
+++ todo/serializers.py
@@ -1,8 +1,18 @@
+from email.policy import default
 from .models import Todo
 from rest_framework import serializers
+from django.contrib.auth.models import User
+from rest_framework import serializers
 
+class UserSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = User
+        fields = ['id', 'first_name', 'last_name', 'username', 'email']
 
 class TodoSerializer(serializers.HyperlinkedModelSerializer):
+    # user = UserSerializer()
+    # user = CurrentUserDefault()
+    user = serializers.PrimaryKeyRelatedField(read_only=True, default=serializers.CurrentUserDefault())
     class Meta:
         model = Todo
-        fields = ['id', 'title', 'description', 'created_at']
+        fields = ['id', 'title', 'description', 'created_at', 'user']
