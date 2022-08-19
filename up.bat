4baeda5d6f5ad0fa75860dbb0bd7092c7a793c4d
Mihai-Andrei Neacsu <crunck78@googlemail.com>
2022-08-19 16:08:27 +0200

    update up.bat file

--- up.bat
+++ up.bat
@@ -1,4 +1,4 @@
 git add .
 git commit -m "%*"
 git push
-chorme "https://www.pythonanywhere.com/user/MihaiAndreiNeacsu/consoles/25245629/"
+chrome "https://www.pythonanywhere.com/user/MihaiAndreiNeacsu/consoles/25245629/"
