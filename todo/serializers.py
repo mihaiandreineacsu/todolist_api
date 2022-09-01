3076be79bc7e56bf1c3234cc77bd3013ef388f4b
Mihai-Andrei Neacsu <crunck78@googlemail.com>
2022-09-01 11:16:34 +0200

    - 11 - Funktionen in Serializer nutzen

--- todo/serializers.py
+++ todo/serializers.py
@@ -4,10 +4,10 @@ from rest_framework import serializers
 from django.contrib.auth.models import User
 from rest_framework import serializers
 
-class UserSerializer(serializers.HyperlinkedModelSerializer):
-    class Meta:
-        model = User
-        fields = ['id', 'first_name', 'last_name', 'username', 'email']
+# class UserSerializer(serializers.HyperlinkedModelSerializer):
+#     class Meta:
+#         model = User
+#         fields = ['id', 'first_name', 'last_name', 'username', 'email']
 
 class TodoSerializer(serializers.HyperlinkedModelSerializer):
     # user = UserSerializer()
@@ -15,4 +15,4 @@ class TodoSerializer(serializers.HyperlinkedModelSerializer):
     user = serializers.PrimaryKeyRelatedField(read_only=True, default=serializers.CurrentUserDefault())
     class Meta:
         model = Todo
-        fields = ['id', 'title', 'description', 'created_at', 'user']
+        fields = ['id', 'title', 'description', 'created_at', 'user', 'time_passed']
